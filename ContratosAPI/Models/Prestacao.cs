58f2444d6227bba353b339d2d17cf86b5d9f5ef4
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2021-01-06 10:52:30 -0300

    Adicionando a entidade Prestação

---
+++ ContratosAPI/Models/Prestacao.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ContratosAPI.Models
+{
+    public class Prestacao
+    {
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public int ContratoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public DataType DataVencimento { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public DataType DataPagamento { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public int Valor { get; set; }
+
+        public string Status { get; set; }
+    }
+}
+
+
+
+
+
+//Entidade Prestação: contrato, data vencimento, data pagamento, valor, status (Aberta, Baixada, Atrasada).
